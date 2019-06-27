c22eab79079af6abfbe5553a366f046423e9f420
MomchilZanev <40828721+MomchilZanev@users.noreply.github.com>
2019-06-27 18:07:54 +0300

    Missing from previous comit

---
+++ MovieDatabase/MovieDatabase.Web/ViewModels/Movie/MovieDetailsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieDatabase.Web.ViewModels.Movie
+{
+    public class MovieDetailsViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Description { get; set; }
+
+        public int Length { get; set; }
+
+        public string Genre { get; set; }
+
+        public string CoverImageLink { get; set; }
+
+        public double Rating { get; set; }
+
+        public string Director { get; set; }
+
+        public List<MovieCastViewModel> Cast { get; set; }
+
+        public List<MovieReviewViewModel> Reviews { get; set; }
+    }
+}

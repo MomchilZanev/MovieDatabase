598efa14aa4e0d747c88f26409b3ba57e6dedd05
MomchilZanev <40828721+MomchilZanev@users.noreply.github.com>
2019-08-10 12:35:27 +0300

    Fixed small bug in account manage, added more partial views

--- MovieDatabase/MovieDatabase.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ MovieDatabase/MovieDatabase.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MovieDatabase.Common;
@@ -9,7 +8,6 @@ using MovieDatabase.Models.CustomValidationAttributes;
 using MovieDatabase.Services.Contracts;
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace MovieDatabase.Web.Areas.Identity.Pages.Account.Manage
@@ -18,18 +16,15 @@ namespace MovieDatabase.Web.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<MovieDatabaseUser> userManager;
         private readonly SignInManager<MovieDatabaseUser> signInManager;
-        private readonly IEmailSender emailSender;
         private readonly IAvatarService avatarService;
 
         public IndexModel(
             UserManager<MovieDatabaseUser> userManager,
             SignInManager<MovieDatabaseUser> signInManager,
-            IEmailSender emailSender,
             IAvatarService avatarService)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
-            this.emailSender = emailSender;
             this.avatarService = avatarService;
         }
 
@@ -78,15 +73,17 @@ namespace MovieDatabase.Web.Areas.Identity.Pages.Account.Manage
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
             var user = await userManager.GetUserAsync(User);
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                var userName = await userManager.GetUserNameAsync(user);
+                Username = userName;
+                return Page();
             }
 
             var email = await userManager.GetEmailAsync(user);
@@ -108,37 +105,6 @@ namespace MovieDatabase.Web.Areas.Identity.Pages.Account.Manage
 
             await signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
-            return RedirectToPage();
-        }
-
-        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            var user = await userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
-            }
-
-
-            var userId = await userManager.GetUserIdAsync(user);
-            var email = await userManager.GetEmailAsync(user);
-            var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
-            var callbackUrl = Url.Page(
-                "/Account/ConfirmEmail",
-                pageHandler: null,
-                values: new { userId = userId, code = code },
-                protocol: Request.Scheme);
-            await emailSender.SendEmailAsync(
-                email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-            StatusMessage = "Verification email sent. Please check your email.";
             return RedirectToPage();
         }
     }
